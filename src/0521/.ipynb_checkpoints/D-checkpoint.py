c0b2045692608109866ee9de5809fda90868a362
kuuumaaaa <67354740+kuuumaaaa@users.noreply.github.com>
2022-05-21 22:40:26 +0900

    0521

---
+++ src/0521/.ipynb_checkpoints/D-checkpoint.py
@@ -0,0 +1,29 @@
+def main():
+    n = int(input())
+    l = list(map(int, input().split()))    
+    #リスト内包表記
+    A = [str(input()) for _ in range(n)]
+    tall = 0
+    for i in range(1,10):
+        a = []
+        counter = 0
+        t = 0
+        for N in range(len(A)):
+            xxx = A[N].index(str(i))
+            a.append(xxx)
+            if i in a:
+                counter = xxx + 10
+            else:
+                counter = xxx
+            if t < counter:
+                t = counter
+        if i == 1 or tall > t:
+            tall = t
+    print(tall)
+    return 
+        
+ 
+if __name__ == '__main__':
+    main()
+
+
